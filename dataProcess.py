2d32d2e412d1a1956130f0ad94c38863185b8cca
chanlo <woluch@foxmail.com>
2018-03-12 10:21:13 +0800

    update

--- dataProcess.py
+++ dataProcess.py
@@ -12,12 +12,35 @@ import json
 import os
 import re
 import tarfile
-import temfile
+import tempfile
 import numpy as np
 np.random.seed(1337)    # for reproducibility
 
 class dataProcess(object):
-    def __init__(self):
+    def __init__(self,filename):
+        self.filename = filename
         pass
 
+    def getData(self):
+        pass
+
+    def getSentence(self):
+        for i,line in enumerate(open(self.filename)):
+            data = json.loads(line)
+            print(i)
+            print(line)
+            print(data)
+            if i >= 2:
+                break
+            
+
     def extractTokenFromBinaryParse(self):
+        return self.parse.replace('(',' ').replace(')',' ').replace('-LRB-','(').replace('-RRB-',')').split()
+    
+
+if __name__ == '__main__':
+    trainData = '../corpus/snli/snli_1.0_train.jsonl'
+    devData = '../corpus/snli/snli_1.0_dev.jsonl'
+    testData = '../corpus/snli/snli_1.0_test.jsonl'
+    dataProcess = dataProcess(testData)
+    dataProcess.getSentence()

8df8789cf83031797f99e4955e1aa1c91a7dea57
chanlo <woluch@foxmail.com>
2018-03-19 23:01:46 +0800

    update

--- model.py
+++ model.py
@@ -1,9 +1,21 @@
 #coding:utf-8
+'''
+Network model code for SNLI corpus.
+==================
+author: ChanLo
+e-mail.com: chanlo@protonmail.ch
+==================
+...
+'''
 
 import keras
 
 class DL_Model(object):
+    def __init__:
+        pass
     
+    def funcname(self, parameter_list):
+        pass
 
 if __name__ == '__main__':
     pass
--- snliMain.py
+++ snliMain.py
@@ -34,14 +34,32 @@ process = dataProcess.data_process()
 train_data = process.get_data(train_data_path)
 dev_data = process.get_data(dev_data_path)
 test_data = process.get_data(test_data_path)
+
 tokenizer = Tokenizer(lower=False, filters='')
-tokenizer.fit_on_texts(training[0] + training[1])
+tokenizer.fit_on_texts(train_data[0] + train_data[1])
 
 VOCAB = len(tokenizer.word_counts) + 1
 
 print('RNN / Embed / Sent = {}, {}, {}'.format(RNN, EMBED_HIDDEN_SIZE, SENT_HIDDEN_SIZE))
 print('GloVe / Trainable Word Embeddings = {}, {}'.format(USE_GLOVE, TRAIN_EMBED))
 
-to_seq = lambda x: pad_sequences(tokenizer.text_to_sequences(x), maxlen=MAX_LEN)
+to_seq = lambda x: pad_sequences(tokenizer.texts_to_sequences(x), maxlen=MAX_LEN)
 prepare_data = lambda data: (to_seq(data[0]), to_seq(data[1]), data[2])
 
+train = prepare_data(train_data)
+validation = prepare_data(dev_data)
+test = prepare_data(test_data)
+
+print('Build model...')
+print('Vocab size =', len(tokenizer.word_counts)+1)
+
+GLOVE_STORE = 'precomputed_glove.weights'
+if USE_GLOVE:
+    if not os.path.exists(GLOVE_STORE = '.npy'):
+        print('Computing Glove')
+
+        embedding_index = {}
+        file = open('../Word2Vec/Glove/glove.840B.300d.txt')
+        for line in f:
+            values = line.split(' ')
+            word = values[0]
